7d0940ddd8b04b0b2f6b5a9005d28ee8752ba9e5
daniel <mahmoodidanial2@gmail.com>
2023-12-19 20:43:33 +0330

    fix: remove unnecessary codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import "./App.css";
-import useListOfItems from "./Hook/useListOfItems.tsx";
 import ListOfItems from "./components/ListOfItems.tsx";
 // import {useQuery, useQueryClient} from "@tanstack/react-query";
 // import {QueryClient} from "@tanstack/react-query/build/modern";
 // import DataList from "./session02/DataList.tsx";
 // import {useState} from "react";
-import AdsContainer from "./session05/AdsContainer.tsx";
+// import AdsContainer from "./session05/AdsContainer.tsx";
 
 function App() {
   // const [flag, setFlag] = useState(true)
   return (
     <>
       {/* <AdsContainer></AdsContainer> */}
-      <ListOfItems/>
+      <ListOfItems />
       <div>new exercise</div>
     </>
   );
